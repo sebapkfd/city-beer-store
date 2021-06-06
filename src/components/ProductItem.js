562b7aacd4bfca91cc04e756cc47454a434ebcdd
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-05 20:10:07 -0400

    Added basic style to navbar and added information to product item

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -7,10 +7,16 @@ const ProductItem = (props) => {
         selectItem(item);
     }
 
+    const unit = (item[1]['units-in-pack'] === 6) ? 'pack' : 'Unidad;'
+
     return (
         <div className={'product-item'}>
             <button onClick={(e) => addItem(e)}>add</button>
+            {/* <img src={item[1].image}
+                alt={item[1].name}/> */}
             <p>{item[1].name}</p>
+            <p>{item[1]['units-in-pack']} {unit}</p>
+            <p>${item[1].price}</p>
         </div>
     )
 }
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -4,6 +4,7 @@ const ProductPage = (props) => {
     const {items, selectItem} = props;
     const {id} = useParams();
     const item = items[id];
+    // console.log(item.image);
 
     const addItem = (e) => {
         e.preventDefault();
@@ -13,6 +14,8 @@ const ProductPage = (props) => {
     return (
         <div>
             <p>{item.name}</p>
+            {/* <img src={item.image}
+                alt={item.name}/> */}
             <button onClick={(e) => addItem(e)}>add</button>
         </div>
     )
