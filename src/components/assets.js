90b1231bc560dc87e3fecb3c72deac5b7bf894c7
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-06 18:59:12 -0400

    Fixed urls

--- src/components/assets.js
+++ src/components/assets.js
@@ -1,11 +1,8 @@
 const assets = {
     'cc0f2086-2dc7-4bec-b35b-9079780d593c': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/microdosis.png',
-    'b65c4d1a-5d22-4375-bf20-44ba81d0c1d3': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/west-coast.png',
+    'b65c4d1a-5d22-4375-bf20-44ba81d0c1d3': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/westcoast.png',
     'b69c5b19-0615-4913-ac92-84609d50edd9': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/cloud-juice.jpeg',
     '9a6dc317-69d8-499a-911d-baa92e52ebda': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/wiggle-room.jpeg'
 }
 
 export default assets;
-
-// https://ibb.co/cQsbgz5
-// https://ibb.co/hgJD81C
