af4c766016034cc702614c1c3e4d3e6775abe593
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-06 18:57:35 -0400

    Changed west coast img

--- src/components/assets.js
+++ src/components/assets.js
@@ -1,8 +1,8 @@
 const assets = {
     'cc0f2086-2dc7-4bec-b35b-9079780d593c': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/microdosis.png',
     'b65c4d1a-5d22-4375-bf20-44ba81d0c1d3': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/west-coast.png',
-    'b69c5b19-0615-4913-ac92-84609d50edd9': 'https://www.tamango.cl/img/uploads/wjnlytubh.jpeg',
-    '9a6dc317-69d8-499a-911d-baa92e52ebda': 'https://www.tamango.cl/img/uploads/xz6j7c582.jpeg'
+    'b69c5b19-0615-4913-ac92-84609d50edd9': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/cloud-juice.jpeg',
+    '9a6dc317-69d8-499a-911d-baa92e52ebda': 'https://raw.githubusercontent.com/sebapkfd/city-beer-store/main/public/assets/wiggle-room.jpeg'
 }
 
 export default assets;
