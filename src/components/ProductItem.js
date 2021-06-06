7eca4f5fdfb4650410b49b97be25f5b17d1ff653
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-06 00:04:14 -0400

    Added icons to ProductItem

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -7,11 +7,15 @@ const ProductItem = (props) => {
         selectItem(item);
     }
 
-    const unit = (item[1]['units-in-pack'] === 6) ? 'pack' : 'Unidad;'
+    const unit = (item[1]['units-in-pack'] === 6) ? 'pack' : 'Unidad';
+
+    const addButton = <span class="material-icons">
+    shopping_bag
+    </span>
 
     return (
         <div className={'product-item'}>
-            <button onClick={(e) => addItem(e)}>add</button>
+            <button onClick={(e) => addItem(e)}>{addButton}</button>
             {/* <img src={item[1].image}
                 alt={item[1].name}/> */}
             <p>{item[1].name}</p>
